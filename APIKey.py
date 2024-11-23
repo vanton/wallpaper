f930311bddda7a8053a140f01af294240a25cb1b
vanton <vanton.work@gmail.com>
2024-11-23 16:15:33 +0800

    Update APIKey.py to allow empty APIKey and clarify usage instructions

--- APIKey.py
+++ APIKey.py
@@ -1,12 +1,13 @@
 # 填写自己的 APIKey
 # Fill in your APIKey
-APIKey = "12345678901234567890123456789012"
+APIKey = ""
 
-'''
+"""
 打开以下页面，获取 APIKey。必须先登录。
+APIKey 可以为空，为空时无法下载 NSFW 内容。
 https://wallhaven.cc/settings/account
 
 API Key
 Your API key can be used to grant other apps access to some of your account settings.
 See our API documentation for more.
-'''
+"""
