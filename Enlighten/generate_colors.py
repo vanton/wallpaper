8163693e4511422ea314ee81dacf33cfd7889c87
vanton <vanton.work@gmail.com>
2024-12-07 17:21:26 +0800

    🌈 style: update generate_colors.py

--- Enlighten/generate_colors.py
+++ Enlighten/generate_colors.py
@@ -6,10 +6,10 @@ def print_color(text, r, g, b, desc=""):
     print(f"\033[38;2;{r};{g};{b}m{text}\033[0m {desc}")
 
 
-def hsl2rgb(h: float, s: float, l: float) -> tuple[int, ...]:
+def hls2rgb(h: float, l: float, s: float) -> tuple[int, ...]:
     # 当饱和度大于0时，进行颜色转换
     if s > 0:
-        colors = colorsys.hls_to_rgb(h, s, l)
+        colors = colorsys.hls_to_rgb(h, l, s)
         red, blue, green = [int(x * 255) for x in colors]
         rgb = (red, blue, green)
     else:
@@ -21,11 +21,11 @@ def hsl2rgb(h: float, s: float, l: float) -> tuple[int, ...]:
 def generate_colors(num_colors: int) -> list[tuple[int, ...]]:
     colors: list[tuple[int, ...]] = []
     for i in range(num_colors):
-        # 通过HSL模型生成颜色，H(色相)在0-1之间，S(饱和度)和L(明度)设置为0.5
+        # 通过HLS模型生成颜色，H(色相)在0-1之间，L(明度)和S(饱和度)设置为0.5
         h = i / num_colors  # 计算色相
-        s = 0.5  # 饱和度
         l = 0.5  # 明度
-        rgb = hsl2rgb(h, s, l)
+        s = 0.5  # 饱和度
+        rgb = hls2rgb(h, l, s)
         colors.append(rgb)
     return colors
 
